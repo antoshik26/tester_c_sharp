6e665eb886cd14d503fb35c8280c598dfab74a41
unknown <antoshik_26@mail.ru>
2021-05-25 17:17:22 +0300

    dey00

---
+++ ex00/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  sum;
+            double rate;
+            int     term;
+            int     selectedMonth;
+            double  payment;
+            double  a;
+            double  Percentages;
+            double  payment_per_month;
+            double  overpayment_1;
+            double  overpaymeny_2;
+            int     i;
+
+            overpayment_1 = 0;
+            overpaymeny_2 = 0;
+            
+            if ((args.Length > 6) || (args.Length < 6))
+            {
+                Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+                Environment.Exit(-1);
+            }
+            for (i = 1; i < args.Length; i++)
+            {
+                foreach(var simvol in args[i])
+                {
+                    if (!char.IsDigit(simvol))
+                    {
+                        Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+                        Environment.Exit(-1);
+                    }
+                }
+            }
+            sum = Convert.ToDouble(args[1]);
+            rate = Convert.ToDouble(args[2]);
+            term = Convert.ToInt32(args[3]);
+            selectedMonth = Convert.ToInt32(args[4]);
+            payment = Convert.ToDouble(args[5]);
+            a = rate / 12 / 100;
+            payment_per_month = (sum * a * (Math.Pow((1 + a), term))) / ((Math.Pow((1 + a), term)) - 1);
+            Console.WriteLine(payment_per_month);
+            i = 1;
+            while (i <= term)
+            {
+                Console.WriteLine(i);
+                if (i == selectedMonth)
+                {
+                    sum = sum - payment;
+                    a = rate / 12 / 100;
+                    payment_per_month = (sum * a * (Math.Pow((1 + a), (6)))) / ((Math.Pow((1 + a), (6))) - 1);
+                    Console.WriteLine(payment_per_month);
+                }
+                Percentages = (sum * rate * DateTime.DaysInMonth(2021, i)) / (100 * 365);
+                Console.WriteLine(Percentages);
+                overpayment_1 = overpayment_1 + Percentages;
+                sum = sum - payment_per_month;
+                i++;
+            }
+            Console.WriteLine("Переплата при уменьшении платежа: {0}p.", overpayment_1);
+            sum = Convert.ToDouble(args[1]);
+            rate = Convert.ToDouble(args[2]);
+            term = Convert.ToInt32(args[3]);
+            selectedMonth = Convert.ToInt32(args[4]);
+            payment = Convert.ToDouble(args[5]);
+            a = rate / 12 / 100;
+            payment_per_month = (sum * a * (Math.Pow((1 + a), term))) / ((Math.Pow((1 + a), term)) - 1);
+            i = 1;
+            while (sum >= 0)
+            {   
+                Console.WriteLine(i);
+                if (i == selectedMonth)
+                {
+                    sum = sum - payment;
+                }
+                Percentages = (sum * rate * DateTime.DaysInMonth(2021, i)) / (100 * 365);
+                Console.WriteLine(Percentages);
+                overpaymeny_2 = overpaymeny_2 + Percentages;
+                sum = sum - payment_per_month;
+                i++;
+            }
+            Console.WriteLine("Переплата при уменьшении платежа: {0}p.", overpaymeny_2);
+            if (overpayment_1 > overpaymeny_2)
+            {
+                Console.WriteLine($"Уменьшение платежа выгоднее уменьшения срока на {overpayment_1 - overpaymeny_2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Уменьшение срока выгоднее уменьшения платежа на {overpaymeny_2 - overpaymeny_1}.");
+            }
+            if (overpayment_1 == overpaymeny_2)
+            {
+                Console.WriteLine("Переплата одинакова в обоих вариантах.");
+            }
+        }
+    }
---
+++ ex01/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+
+namespace program_2
+{
+    class Program
+    { 
+        static int LevenshteinDistance(string firstWord, string secondWord)
+        {
+            var n = firstWord.Length + 1;
+            var m = secondWord.Length + 1;
+            var matrixD = new int[n, m];
+
+            const int deletionCost = 1;
+            const int insertionCost = 1;
+
+            for (var i = 0; i < n; i++)
+            {
+                matrixD[i, 0] = i;
+            }
+
+            for (var j = 0; j < m; j++)
+            {
+                matrixD[0, j] = j;
+            }
+
+            for (var i = 1; i < n; i++)
+            {
+                for (var j = 1; j < m; j++)
+                {
+                    var substitutionCost = firstWord[i - 1] == secondWord[j - 1] ? 0 : 1;
+
+                    matrixD[i, j] = Math.Min(matrixD[i - 1, j] + deletionCost, matrixD[i, j - 1] + insertionCost);
+                    matrixD[i, j] = Math.Min(matrixD[i, j], matrixD[i - 1, j - 1] + substitutionCost);
+                }
+            }
+            return matrixD[n - 1, m - 1];
+        }
+
+        static void Main(string[] args)
+        {
+            string read_name;
+            string[] array_name;
+            int i;
+            int j;
+            int result;
+            string yes_or_no;
+
+             
+            array_name = new string[5163]; 
+            Console.WriteLine("Enter name:");
+            read_name = Console.ReadLine();
+            if (read_name == "")
+            {
+                Console.WriteLine("Your name was not found.");
+                Environment.Exit(-1);
+            }
+            foreach(var simvol in read_name)
+            {
+                if (!char.IsLetter(simvol))
+                {
+                    Console.WriteLine("Your name was not found.");
+                    Environment.Exit(-1);
+                }
+            }
+            StreamReader file = new StreamReader("array_name.txt");
+            i = 0;
+            while (!file.EndOfStream)
+            {
+                array_name[i] = file.ReadLine();
+                i++;
+            }
+            file.Close();
+            i = 0;
+            while(i < array_name.Length)
+            {
+                if (array_name[i] == read_name)
+                {
+                    Console.WriteLine("Hello, {1}", array_name[i]);
+                    Environment.Exit(0);
+                }
+                result = LevenshteinDistance(read_name, array_name[i]);
+                if (result < 3)
+                {
+                    Console.WriteLine($"Did you mean: {array_name[i]}? Y/N");
+                    yes_or_no = Console.ReadLine();
+                    if (yes_or_no == "Y")
+                    {
+                        Console.WriteLine($"Hello, {array_name[i]}");
+                        Environment.Exit(0);
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine("Your name was not found.");
+            Environment.Exit(-1);
+        }
+    }
+}
